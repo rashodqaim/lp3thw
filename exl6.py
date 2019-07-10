b4d981ed86eec43db3cbb89c373bfae356fa93cf
Rashod Qaim <rashod@basketsavings.com>
2019-07-10 10:23:14 -0400

    Added file

---
+++ exl6.py
@@ -0,0 +1,28 @@
+#variable called types of people
+types_of_people = 10
+#varibale called X which has the variable types of people in it
+x =f"There are {types_of_people} types of people."
+# variable called binary
+binary = "binary"
+# variable called do not
+do_not ="don't"
+# variable called y which has the variables in the string
+y =f"Those who know {binary} and those who {do_not}."
+# print the variables
+print(x)
+print(y)
+# print I said with the variable x/y in the string
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+# variable called hilarious that is false
+hilarious = False
+# variable called joke_evaluation and the string has nothing in it
+joke_evaluation = "isn't that joke so funny?! {}"
+# print variable joke_evaluation and format to any other variable this one is hilarious variable
+print(joke_evaluation.format(hilarious))
+# variable called w
+w = "This is the left side of..."
+# variable called e
+e = "a string with a right side."
+# print variable w and add variablee e to the end
+print(w + e)
