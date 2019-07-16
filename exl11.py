d6bd2cb5b79eae9ef42b05067b51cb00a10f3ac9
Rashod Qaim <rashod@basketsavings.com>
2019-07-16 15:20:19 -0400

    File added'

--- exl11.py
+++ exl11.py
@@ -1,10 +1,19 @@
-# Print statement but when a end='' at the end of line, this tells print to end the line with a newline character and go to the input
-print("How old are you?", end='') #its going to ask you to put in a input
-#variable called age where it ask for the input of the line above
+# Print statement when you have the word "end" at the end of the statement, the program doesn't go to the next time but still runs the code
+print("How old are you?", end='')
+#variable called age where it ask for the input which could be anything
 age = input()
 print("How tall are you?", end='')
 height = input()
 print("How much do you weigh?", end='')
-weight = input()
+weight = int(input())
 #print statement with all the variable inputs
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
+print("What is your favorite animal?", end='')
+animal = input()
+print("How many pets do you have?", end='')
+pets = input()
+print("Enter your name")
+name = input()
+print(f"Your favorite animal is {animal}, you have {pets} pets, and your name is {name}")
