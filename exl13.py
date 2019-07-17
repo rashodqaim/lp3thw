ca7b47485188f6927eab769d74b3da7b05769f37
Rashod Qaim <rashod@basketsavings.com>
2019-07-17 11:09:01 -0400

    more notes

--- exl13.py
+++ exl13.py
@@ -6,6 +6,7 @@ print("the script is called:", script)
 print("your first variable is:", first)
 print("Your second varibale is:", second)
 print("Your third variable is:", third)
+lunch = input("What do you want to eat")
 # I can replace the argv with anything I want when I run it in the terminal
 print(f"V1 {first}, V2 {second}, V3 {third}")
 
@@ -14,6 +15,6 @@ print(f"V1 {first}, V2 {second}, V3 {third}")
 print("the script is called:", Dinner)
 print("your first variable is:", Thai)
 print("Your second varibale is:", Mexican)
-
+print(f"Yes we can have {lunch} for lunch today")
 
 #Notes What ever the order the argv is in that is the order I have to write it in the terminal.
