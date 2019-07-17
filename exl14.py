b2526527d6e89c6289191d466a8c366f05e8685e
Rashod Qaim <rashod@basketsavings.com>
2019-07-17 13:00:12 -0400

    New Chapter

---
+++ exl14.py
@@ -0,0 +1,19 @@
+from sys import argv
+# script is allways the file name
+script, user_name = argv
+prompt = '< ' #This shows when asking for input
+
+print(f"Hi {user_name}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt) # Can also be just ('')
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"""
+alright, so you said {likes} about liking me.
+you live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.""")
