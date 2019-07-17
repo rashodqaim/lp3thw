c20a7eae1a5bab6848629e7d07d05ef9956e0b1f
Rashod Qaim <rashod@basketsavings.com>
2019-07-17 11:05:14 -0400

    Added Notes

---
+++ exl13.py
@@ -0,0 +1,19 @@
+from sys import argv
+#read the WYSS section fro how to fun this
+script, first, second, third, Thai, Mexican, Dinner = argv
+
+print("the script is called:", script)
+print("your first variable is:", first)
+print("Your second varibale is:", second)
+print("Your third variable is:", third)
+# I can replace the argv with anything I want when I run it in the terminal
+print(f"V1 {first}, V2 {second}, V3 {third}")
+
+
+# Dinner, Thai, Mexican = argv
+print("the script is called:", Dinner)
+print("your first variable is:", Thai)
+print("Your second varibale is:", Mexican)
+
+
+#Notes What ever the order the argv is in that is the order I have to write it in the terminal.
