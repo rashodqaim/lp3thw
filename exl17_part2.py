9c577c3b11e6e34f6cadfa9e0214980f204d8abe
Rashod Qaim <rashod@basketsavings.com>
2019-07-30 16:37:10 -0400

    Code in 1 line

---
+++ exl17_part2.py
@@ -0,0 +1,28 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+# print(f"Copying from {from_file} to {to_file}")
+
+
+# in_file = open(from_file).read()
+# indata = in_file.read()
+
+# print(f"The input file is {len(indata)} bytes long")
+
+# print(f"Does the output file exist? {exists(to_file)}")
+# print("Ready, hit RETURN to continue, CTRL-C abort.")
+# input()
+
+# out_file = open(to_file, 'w').write(in_file)
+# out_file.write(in_file)
+
+# print("Alright, all done.")
+
+# out_file.close()
+# in_file.close()
+
+
+#writes codes whole code in one line
+out_file = open(to_file, 'w').write(open(from_file).read())
